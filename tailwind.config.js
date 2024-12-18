c4a108e57158ebd84660ead1e84cd943cf41f272
Dikifebrian24 <Dikifebrian04@gmail.com>
2024-12-18 23:15:27 +0700

    Kisah Kita Graduation

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['index.html'],
+//   theme: {
+//     extend: {
+//       colors: {
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: [
+    "index.html",
+    "fotografer.html",
+    "fotografer1.html",
+    "portofolio.html",
+    "Portofolio1",
+  ],
+  // darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#009379",
+        secondary: "#64748b",
+        third: "#2D2D2D",
+        dark: "#0f172a",
+        color1: "#FF6250",
+        color2: "#F3AFA8",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
